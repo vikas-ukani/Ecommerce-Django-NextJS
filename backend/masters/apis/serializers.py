0a966c15c874e1ef8a4e9813a441dafcd8558e4b
Vikas Ukani <vikasukani5@gmail.com>
2021-10-10 11:53:34 +0530

    DONE: Category and Product model field changes.

--- backend/masters/apis/serializers.py
+++ backend/masters/apis/serializers.py
@@ -6,3 +6,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'description', 'is_active', 'created_at', 'updated_at']
+
+    # @staticmethod
+    # def get_sub_categories(obj):
+    #     return CategorySerializer(obj.categories, many=True, read_only=True).data
