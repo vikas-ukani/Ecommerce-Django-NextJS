51ca56f939615bba3ff93071876a4acdac83e1df
Vikas Ukani <vikasukani5@gmail.com>
2021-10-10 22:30:32 +0530

    DONE: Feature Product UI done...

--- frontend/Components/Products/FeatureProducts.jsx
+++ frontend/Components/Products/FeatureProducts.jsx
@@ -6,31 +6,47 @@ const FeatureProducts = () => {
                     featured products
                 </b>
             </div>
-            <div className="section-center pt-10 sm:grid md:grid-cols-2 xl:grid-cols-1 grid">
-                {[1, 2, 3, 4, 5, 6, 7].map((id) => {
+            <div className="section-center pt-10 sm:grid md:grid-cols-3 xl:grid-cols-3 grid">
+                {[1, 2, 3, 4, 5, 6].map((id) => {
                     return (
-                        <div key={id} className="px-10 py-9 ">
-                            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
-                                <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" className="w-full align-middle rounded-t-lg" />
-
-                                <blockquote className="relative p-8 mb-4">
-                                    <svg height="110" className="-top-1/2 absolute left-0 w-full block h-95-px -top-94-px" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95">
-                                        <polygon points="-30,95 583,95 583,65" className="text-pink-500 fill-current pt-1"></polygon>
-                                    </svg>
-                                    <h4 className="capitalize text-4xl font-bold text-white  float-left "  >
-                                        entertainment center
-                                    </h4>
-                                    <p className="capitalize text-2xl font-bold text-black float-right " >
-                                        $599.99
-                                    </p>
-                                    <p className="text-md font-light mt-2 text-white">
-                                        <footer >
-                                            <b></b>
-
-                                        </footer>
-                                    </p>
-                                </blockquote>
+                        <div key={id} className="px-5 py-5 ">
+                            <div className="max-w-lg mx-auto">
+                                <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-full mb-5">
+                                    <a href="#">
+                                        <img className="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                                    </a>
+                                    <div className="p-5 bg-indigo-100">
+                                        <a href="#">
+                                            <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology acquisitions 2021</h5>
+                                        </a>
+                                        <p className="font-normal text-gray-700 mb-3">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                        <div className="text-center space-x-10">
+                                            <a className="shadow-lg hover:shadow-2xl text-black bg-yellow-500 hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-400 font-large rounded-lg font-black text-lg px-6 py-3 text-center inline-flex items-center" href="#">
+                                                Add to cart
+                                            </a>
+                                            <a className="shadow-lg hover:shadow-2xl text-black bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-400 font-large rounded-lg font-black text-lg px-6 py-3 text-center inline-flex items-center" href="#">
+                                                Buy now
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+                            {/* <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-pink-500">
+                                <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" className="w-full align-middle rounded-t-lg" />
+                                <div className="relative p-4 mb-4">
+                                    <div className="float-left">
+                                        <h3 className="capitalize text-2xl font-bold text-white  "  >
+                                            entertainment center
+                                        </h3>
+                                        <p className="capitalize text-1xl font-bold text-black  " >
+                                            $599.99
+                                        </p>
+                                    </div>
+                                    <div className="float-right">
+                                        <i className="fa fa-heart text-white"></i>H
+                                    </div>
+                                </div>
+                            </div> */}
 
                             {/* <div className="container card-image ">
                                 <img height="100%" width="100%"
