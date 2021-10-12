abfd9cc98708bcca24a1f804b5203aa52e7b5966
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-10-12 13:56:16 +0530

    added login page with states

--- frontend/pages/auth/login.jsx
+++ frontend/pages/auth/login.jsx
@@ -1,7 +1,45 @@
-const Login = () => {
+import React, { useState } from "react";
+
+const Login = () =>
+{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault();
+        console.log(email, password)
+    }
+
     return (
-        <div>
-            Login
+        <div className="min-h-screen py-2 flex flex-col justify-center sm:py-10">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                <div
+                    className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                </div>
+                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                    <div className="max-w-md mx-auto">
+                        <div>
+                            <h1 className="text-2xl font-semibold">Welcome Back !</h1>
+                        </div>
+                        <div className="divide-y divide-gray-200">
+                            <form onSubmit={handleSubmit} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                <div className="relative">
+                                    <input autocomplete="none" value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" required />
+                                    <label for="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                                </div>
+                                <div className="relative">
+                                    <input autocomplete="none" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                    <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                </div>
+                                <div className="relative">
+                                    <button type="submit" className="bg-blue-500 text-white rounded-md px-2 py-1">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
