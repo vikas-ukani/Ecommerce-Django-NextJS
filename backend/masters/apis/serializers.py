52563706addd2319cad214294d38e0a3e1266b03
Vikas Ukani <vikas@maven.one>
2021-10-13 18:17:14 +0530

    WIP: Fetching Projects By Category ID

--- backend/masters/apis/serializers.py
+++ backend/masters/apis/serializers.py
@@ -2,10 +2,10 @@ from masters.models import Category
 from rest_framework import serializers
 
 
-class SubCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('name', 'description')
+# class SubCategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = ('name', 'description')
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -16,20 +16,13 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description', 'is_active',
                   'sub_categories', 'created_at', 'parent',
                   'updated_at']
-        
         depth = 10
 
-    # def get_related_field(self, model_field):
-    #     # Handles initializing the `subcategories` field
-    #     return CategorySerializer()
-
     def get_fields(self):
-            fields = super(CategorySerializer, self).get_fields()
-            fields['sub_categories'] = CategorySerializer(many=True)
-            return fields
+        fields = super(CategorySerializer, self).get_fields()
+        fields['sub_categories'] = CategorySerializer(many=True)
+        return fields
 
     @staticmethod
     def get_sub_categories(obj):
         return CategorySerializer(obj, many=True, read_only=True).data
-
-
